82302dc0356ed15282358ad3bb0b50c560cf7ac7
dcms-social <dcms-social@bk.ru>
2020-10-12 13:51:40 +0300

    Update count.php

--- user/count.php
+++ user/count.php
@@ -1,6 +1,12 @@
-<?
-$koll = mysql_result(mysql_query("SELECT COUNT(*) FROM `user`",$db), 0);
-$k_new = mysql_result(mysql_query("SELECT COUNT(*) FROM `user` where `date_reg` > '$ftime' ",$db), 0);
-if ($_new > 0) $k_new = '<font color="red">+' . $k_new . '</font>'; else $k_new = null;
-echo '(' . $koll . ') ' . $k_new;
-?>
+<?
+
+$koll = mysql_result(mysql_query("SELECT COUNT(*) FROM `user`",$db), 0);
+
+$k_new = mysql_result(mysql_query("SELECT COUNT(*) FROM `user` where `date_reg` > '$ftime' ",$db), 0);
+
+if ($k_new > 0) $k_new = '<font color="red">+' . $k_new . '</font>'; else $k_new = null;
+
+echo '(' . $koll . ') ' . $k_new;
+
+?>
+
